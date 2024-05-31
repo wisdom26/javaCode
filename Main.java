642bb74857dcfb39fe99bcd0db6b67554fcefd47
wisdom26 <42862067+wisdom26@users.noreply.github.com>
2024-05-31 15:54:40 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+  public static void main(String[] args) {
+    Store lemonadeStand = new Store("lemonade", 42, .99);
+    Store cookieShop = new Store("cookies", 12, 3.75);
+    
+    System.out.println("Our first shop sells " + lemonadeStand.productType + " at " + lemonadeStand.inventoryPrice + " per unit.");
+    
+    System.out.println("Our second shop has " + cookieShop.inventoryCount + " units remaining.");
+  }
+}
---
+++ MyProfile.java
@@ -0,0 +1,15 @@
+public class MyProfile {
+	public static void main(String[] args) {   
+                   String name ="vinay";
+                   int age =25;
+                   double desiredSalary=100000;
+                   char gender = 'm';
+                   boolean lookingForJob=true;
+				   System.out.println(name);
+				   System.out.println(age);
+				   System.out.println(desiredSalary);
+				   System.out.println(gender);
+				   System.out.println(lookingForJob);
+
+	}
+}
